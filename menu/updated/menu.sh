273d6c2e4da95436aa96eb376bf00acffd868ccc
wehoi <89265553+wehoi@users.noreply.github.com>
2023-12-28 09:30:06 +0800

    Update menu.sh

--- menu/updated/menu.sh
+++ menu/updated/menu.sh
@@ -1,55 +1,79 @@
 #!/bin/bash
-# Information
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+#information
 OK="${GREEN}[OK]${NC}"
 Error="${RED}[Mistake]${NC}"
-
 clear
-echo -e "────────────────────────────────────────────────────────────"
-echo -e "          JJJ   AAAAA  V   V  AAAAA                   "
-echo -e "           J    A   A  V   V  A   A                   "
-echo -e "           J    AAAAA  V   V  AAAAA                   "
-echo -e "        J  J    A   A   V V   A   A                   "
-echo -e "         JJ     A   A    V    A   A                   "
-echo -e "────────────────────────────────────────────────────────────"
-echo -e "            WELCOME TO FREESCRIPT"
-echo -e "                 Powered by JAVA"
+echo -e "************************************************************"
+echo -e "******************* WELCOME TO JAVANET *********************"
+echo -e "****************** Auto Scrip By jAvaNet *******************"
+echo -e "**LINE:javanet**** Whatsapp:+66934567688 *****FB:javajaja***"
+echo -e "************************************************************"
+echo -e "                    ข้อมูลวีพีเอส                "
+echo -e "************************************************************"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+#clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IPVPS"
+echo -e "ISP-VPS         :  $ISPVPS"
+echo -e "************************************************************"
+echo -e                      เมนูหลัก       
+echo -e "************************************************************"
+echo -e "$NC          1$NC. สร้างบัญชี              "
+echo -e "$NC          2$NC. รีสตาร์ทบริการทั้งหมด          "
+echo -e "$NC          3$NC. ตรวจสอบพอร์ต             "
+echo -e "$NC          4$NC. ตรวจสอบบริการ                 "
+echo -e "$NC          5$NC. ตั้งค่า                  "
+echo -e "$NC          6$NC. ออก                        "
+echo -e "************************************************************"
+echo -e "$NC หากตรวจเจอปัญหาติดต่อแอดมิน Telegram 000000 "  
+echo -e "$NC Copyright freevpnNet " 
+echo -e "************************************************************"
+read -p " โปรดเลือก [ 1 - 6 ] : " menu
+echo -e "************************************************************"
 echo -e ""
-echo -e "           [ หน้าหลัก ]       "
-echo -e "────────────────────────────────────────────────────────────"
-echo -e "1. สร้างบัญชี              "
-echo -e "2. รีสตาร์ทบริการทั้งหมด          "
-echo -e "3. ตรวจสอบพอร์ต             "
-echo -e "4. ตรวจสอบบริการ                 "
-echo -e "5. ตั้งค่า                  "
-echo -e "6. ออก                        "
-echo -e "────────────────────────────────────────────────────────────"
-echo -e "หากตรวจเจอปัญหาติดต่อแอดมิน Telegram https://t.me/yallaaj"
-echo -e "Copyright JAVA"
-read -p "โปรดเลือก [1 - 6] : " menu
-echo -e "────────────────────────────────────────────────────────────"
 case $menu in
-    1)
-        maddxray
-        ;;
-    2)
-        restart-xray
-        ;;
-    3)
-        cek-port
-        ;;
-    4)
-        start-menu
-        ;;
-    5)
-        msettings
-        ;;
-    6)
-        clear
-        exit
-        ;;
-    *)
-        clear
-        menu
-        ;;
+1)
+maddxray
+;;
+2)
+restart-xray
+;;
+3)
+cek-port
+;;
+4)
+start-menu
+;;
+5)
+msettings
+;;
+5)
+clear
+exit
+;;
+*)
+clear
+menu
+;;
 esac
-
+#
